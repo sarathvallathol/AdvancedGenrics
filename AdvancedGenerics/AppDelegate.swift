456a96d200a2a8bea80ea6ddb38178f95bb19e5d
sarath <sarath@askme.in>
2018-07-03 12:34:56 +0530

    genrics exmaple

--- AdvancedGenerics/AppDelegate.swift
+++ AdvancedGenerics/AppDelegate.swift
@@ -18,7 +18,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         
         window = UIWindow()
         window?.makeKeyAndVisible()
-        window?.rootViewController = SampleTableViewController()
+        window?.rootViewController = SomeListController()
         return true
     }
 
--- AdvancedGenerics/SampleTableViewController.swift
+++ AdvancedGenerics/SampleTableViewController.swift
@@ -8,13 +8,14 @@
 
 import UIKit
 
-class SampleTableViewController: UITableViewController {
+class BaseTableViewController<T: BaseCell<U>, U >: UITableViewController {
 
     let cellId = "cellId"
     
+    var items = [U]()
     override func viewDidLoad() {
         super.viewDidLoad()
-       tableView.register(UITableViewCell.self, forCellReuseIdentifier: cellId)
+       tableView.register(T.self, forCellReuseIdentifier: cellId)
 
     }
 
@@ -23,15 +24,61 @@ class SampleTableViewController: UITableViewController {
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 100
+        return items.count
     }
 
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: cellId, for: indexPath)
-        cell.textLabel?.text = "\(indexPath.row)"
+        let cell = tableView.dequeueReusableCell(withIdentifier: cellId, for: indexPath) as! BaseCell<U>
+        cell.item = items[indexPath.row]
         return cell
     }
 
 
+}
+
+class BaseCell<U>: UITableViewCell {
+    
+    var item: U!
+
+}
+struct Dog {
+    
+    let name:String
+    
+}
+class DogCell: BaseCell<Dog> {
+    override var item: Dog!{
+        
+        didSet{
+            textLabel?.text = item.name
+        }
+    }
+    
+}
+class  StringCell: BaseCell<String> {
+    
+    override var item: String!{
+        
+        didSet{
+            textLabel?.text = "\(item)"
+        }
+    }
+    
+}
+class DummyController : BaseTableViewController<StringCell, String> {
+    
+}
+class SomeListController: BaseTableViewController<DogCell, Dog> {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        items = [
+            Dog(name: "woof woof"),
+            Dog(name: "Ruff Ruff")
+        ]
+    }
+    
+  
 }
